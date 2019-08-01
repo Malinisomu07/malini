dd22ed58405d471243441ae271eba7aaeaf203e2
Malinisomu07 <53169056+Malinisomu07@users.noreply.github.com>
2019-08-02 00:17:14 +0530

    Add files via upload

---
+++ holiday.py
@@ -0,0 +1,17 @@
+a=str(input(""))
+if(a=="Sunday"):
+    print("yes")
+elif(a=="Monday"):
+    print("no")
+elif(a=="Tuesday"):
+    print("no")
+elif(a=="Wednesday"):
+    print("no")
+elif(a=="Thursday"):
+    print("no")
+elif(a=="Friday"):
+    print("no")
+elif(a=="sunday"):
+    print("yes")
+else:
+    print("Invalid day")
