1e004b42e1c4b7a40333b0e6b563a802cdccce3c
Malinisomu07 <53169056+Malinisomu07@users.noreply.github.com>
2019-08-13 04:04:11 -0700

    Create Player5.py

---
+++ Player5.py
@@ -0,0 +1,5 @@
+import sys, string, math
+a =input()
+b = ['','I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX','XXI','XXII','XXIII','XXIV','XXV','XXVI','XXVII','XXVIII','XXIX','XXX']
+mk = b.index(a)
+print(mk)
